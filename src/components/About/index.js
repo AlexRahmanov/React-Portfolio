9ae503c1ddb486d16e6c1f0ffbbc6de63dde42ef
Alexander <alexr722@gmail.com>
2022-07-10 13:53:22 -0400

    updated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ function About() {
         id="about-photo"
       />
       <p style={{marginLeft: "60px"}}>
-      Hey there! I'm Alex
+      Hey there! I'm Alex.
       <br></br>
       A highly motivated full-stack web developer, seeking for new opportunity to start my career as a web developer! Able to create responsive, modern, and user-friendly applications and web pages using mobile first approach. Passionate about what is possible to achieve using the latest technologies and eager to learn new things!
     
